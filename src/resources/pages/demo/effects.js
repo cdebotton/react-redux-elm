9c0857def97e26c58921ef504a6ccb0d7e642ea6
Christian de Botton <cdebotton@gmail.com>
2016-09-07 01:10:08 -0400

    Type check effects

--- src/resources/pages/demo/effects.js
+++ src/resources/pages/demo/effects.js
@@ -2,12 +2,24 @@
 
 import fetch from 'isomorphic-fetch';
 
-type FetchItems = (count: number) => number[];
-export const fetchItems: FetchItems = count => fetch(`/api/test?count=${count}`)
-  .then(response => {
+type JSON = {
+  string: string | number | boolean | null,
+};
+
+type Response = {
+  status: number,
+  json: () => JSON,
+};
+
+type Body = {
+  items: number[],
+};
+
+export default (count: number): number[] => fetch(`/api/test?count=${count}`)
+  .then((response: Response) => {
     if (response.status > 400) {
       throw new Error('Error while fetching from the server.');
     }
     return response.json();
   })
-  .then(body => body.items);
+  .then((body: Body) => body.items);
